97c30d6add5df3350aff050f2a74c6211494013c
Nikhil Bhola <nikhil>
2017-03-12 22:56:35 +0530

    First Commit

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Nikhil on 12/03/17.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular2 App</h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Nikhil on 12/03/17.
+ */
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+
+platform.bootstrapModule(AppModule);
